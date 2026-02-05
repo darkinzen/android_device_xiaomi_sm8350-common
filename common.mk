2ac3b81021aa0661573c2ec580be619be418d136
hseyinblgc <h.bilgic1818@gmail.com>
2026-02-05 19:53:06 +0300

    sm8350-common: change charging_control_supports_bypass soong variable to bool
    FAILED: out/soong/build.lineage_mars.ninja
    cd "$(dirname "out/host/linux-x86/bin/soong_build")" && BUILDER="$PWD/$(basename "out/host/linux-x86/bin/soong_build")" && cd / && env -i  "$BUILDER"     --top "$TOP"     --soong_out "out/soong"     --out "out"     --soong_variables out/soong/soong.lineage_mars.variables -o out/soong/build.lineage_mars.ninja --kati_suffix -lineage_mars -l out/.module_paths/Android.bp.list --available_env out/soong/soong.environment.available --used_env out/soong/soong.environment.used.lineage_mars.build Android.bp
    error: hardware/lineage/interfaces/health/aidl/default/Android.bp:62:11: module "vendor.lineage.health-service.default" variant "android_vendor_arm64_armv8-2a-dotprod": cflags: Expected all branches of a select on condition soong_config_variable("lineage_health", "charging_control_supports_bypass") to have type string, found bool
    16:48:35 soong bootstrap failed with: exit status 1

--- common.mk
+++ common.mk
@@ -260,7 +260,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
 # Media
 PRODUCT_COPY_FILES += \
